b4bafb7c59c1b80149960bb7cd1c7dff1570526d
ramkrishna-xyz <ramkrishnadeveloper1@gmail.com>
2025-12-23 15:43:26 +0000

    chore: configure database and github auth for production

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,12 @@
 import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = global as unknown as { prisma: any };
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     log: ["query"],
-  });
+  }).$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
